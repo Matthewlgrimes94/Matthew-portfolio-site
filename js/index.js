373cb43fa7ee04c2b4b95cb70bf9e07930796f53
M Grimes <matthewlgrimes@gamil.com>
2020-01-22 16:52:17 -0700

    basic structure

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.carousel.carousel-slider').carousel({
+        indicators: true
+      });
+    $('.next').click(function() {
+    $('.carousel.carousel-slider').carousel('next');
+    });
+    $('.prev').click(function() {
+    $('.carousel.carousel-slider').carousel('prev');
+    });
+  });
